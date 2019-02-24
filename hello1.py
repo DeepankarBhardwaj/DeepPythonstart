46b355e2e0faf224bd49aa925e06c39784cfcbe6
DEEPANKAR BHARDWAJ <deepankar@deepankars-mbp.mynet>
2019-02-24 16:05:57 +0000

    Second  Change

--- hello1.py
+++ hello1.py
@@ -2,4 +2,4 @@ x = float (input ("Enter first number"))
 y = float (input ("Enter second number"))
 z = float (input ("enter third number"))
 
-print ("maximum value is ",  max(x,y,z))
+print ("maximum value is "   ,  max(x,y,z))
